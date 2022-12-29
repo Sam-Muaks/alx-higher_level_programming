d3c64186590b100ecad5135a19df5cdbca70c3ea
Mohri Samuel Muakpo <mohrimuakpo@gmail.com>
2022-12-29 12:27:22 -0800

    executables

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
 
+
 def uniq_add(my_list=[]):
-    result = 0
-    for x in set(my_list):
-        result += x
-    return resul:it
+    new = set(my_list)
+    res = 0
+    for i in new:
+        res += i
+    return res
